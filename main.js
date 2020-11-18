c410828502106449b2513098bc7ad33a7087da62
trootski <troot1@gmail.com>
2020-11-18 20:15:21 +0000

    deploy: trootski/src.trootski.github.io@b783e9026131ab968ee5493f50eb438fc5acc088

--- main.js
+++ main.js
@@ -3,14 +3,25 @@ window.addEventListener('DOMContentLoaded', (event) => {
     // https://bl.ocks.org/mpetroff/4666657beeb85754611f
 
     // Navbar and dropdowns
-    const toggle = document.getElementsByClassName('navbar-toggle')[0],
-          collapse = document.getElementsByClassName('navbar-collapse')[0];
+    const navbarToggle = document.getElementsByClassName('navbar-toggle')[0],
+          navbarCollapse = document.getElementsByClassName('navbar-collapse')[0]
+          sidebarCollapse = document.getElementById('sidebarCollapse')
+          sidebarToggle = document.getElementsByClassName('sidebar-toggle')[0];
 
     // Toggle if navbar menu is open or closed
-    function toggleMenu() {
-        collapse.classList.toggle('collapse');
+    const toggleNavbar = ele => () => {
+        ele.classList.toggle('collapse');
+    }
+
+    const toggleSidebar = ele => () => {
+        if (ele.classList.contains('d-none')) {
+            ele.classList.remove('d-none', 'd-sm-block');
+        } else {
+            ele.classList.toggle('collapse');
+        }
     }
 
     // Event listeners
-    toggle.addEventListener('click', toggleMenu, false);
+    navbarToggle.addEventListener('click', toggleNavbar(navbarCollapse), false);
+    sidebarToggle.addEventListener('click', toggleSidebar(sidebarCollapse), false);
 });
